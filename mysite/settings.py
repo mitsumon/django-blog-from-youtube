5ce0bdc20bb575217d9116036cdccc8a6a05ec8b
mondo <m.mondo@it-tech.co.jp>
2021-10-28 16:26:57 +0900

    added delete post and login functions

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,10 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'widget_tweaks',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 
     'app.apps.AppConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
